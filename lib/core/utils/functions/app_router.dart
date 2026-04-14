45810a2b50ec1b0f93025fd8e793b3fd5bdd191a
Zeiad Ramadan <zeiadramadan6@gmail.com>
2026-04-14 19:45:42 +0200

    add sign up view ui

--- lib/core/utils/functions/app_router.dart
+++ lib/core/utils/functions/app_router.dart
@@ -1,12 +1,18 @@
 import 'package:go_router/go_router.dart';
+import 'package:movie_verse_app/features/auth/presentation/views/sign_up_view.dart';
 import 'package:movie_verse_app/features/auth/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kSplashView = '/splashView';
+  static const kSignUpView = '/signUpView';
 
   static final router = GoRouter(
     routes: [
       GoRoute(path: '/', builder: (context, state) => const SplashView()),
+      GoRoute(
+        path: kSignUpView,
+        builder: (context, state) => const SignUpView(),
+      ),
     ],
   );
 }
