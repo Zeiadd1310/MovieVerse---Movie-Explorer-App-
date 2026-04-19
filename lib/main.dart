b57acd23bd9a72ab774efc856cf52d7f8337c5ec
yasminemoataz7 <yasminemoataz13@gmail.com>
2026-04-19 15:58:16 +0200

    resolved merge conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; // Import this!
-import 'package:movie_verse_app/features/movie_details/presentation/views/movie_details_view.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movie_verse_app/core/utils/functions/app_router.dart';
 
 void main() {
   runApp(const MovieVerseApp());
@@ -11,16 +12,23 @@ class MovieVerseApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        // This applies Inter to the entire app's text system
-        textTheme: GoogleFonts.interTextTheme(
-          Theme.of(context).textTheme,
-        ).apply(bodyColor: Colors.white, displayColor: Colors.white),
+    return ScreenUtilInit(
+      designSize: const Size(390, 908),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          textTheme: GoogleFonts.interTextTheme(
+            Theme.of(context).textTheme,
+          ).apply(
+            bodyColor: Colors.white,
+            displayColor: Colors.white,
+          ),
+        ),
       ),
-      home: MovieDetailsScreen(),
     );
   }
 }
