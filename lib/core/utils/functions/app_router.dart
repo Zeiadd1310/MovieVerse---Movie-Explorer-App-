5ef63f8a8f0b2f99350e59c302cb13d6f6544f57
Zeiad Ramadan <zeiadramadan6@gmail.com>
2026-05-12 17:21:26 +0300

    adding routes

--- lib/core/utils/functions/app_router.dart
+++ lib/core/utils/functions/app_router.dart
@@ -2,11 +2,15 @@ import 'package:go_router/go_router.dart';
 import 'package:movie_verse_app/features/auth/presentation/views/sign_in_view.dart';
 import 'package:movie_verse_app/features/auth/presentation/views/sign_up_view.dart';
 import 'package:movie_verse_app/features/auth/presentation/views/splash_view.dart';
+import 'package:movie_verse_app/features/movie_details/presentation/views/movie_details_view.dart';
+import 'package:movie_verse_app/features/movie_details/presentation/views/review_rating_screen.dart';
 
 abstract class AppRouter {
   static const kSplashView = '/splashView';
   static const kSignUpView = '/signUpView';
   static const kSignInView = '/signInView';
+  static const kMovieDetailsView = '/movieDetailsView';
+  static const kReviewRatingScreen = '/reviewRatingScreen';
 
   static final router = GoRouter(
     routes: [
@@ -18,6 +22,14 @@ abstract class AppRouter {
       GoRoute(
         path: kSignInView,
         builder: (context, state) => const SignInView(),
+      ),
+      GoRoute(
+        path: kMovieDetailsView,
+        builder: (context, state) => const MovieDetailsView(),
+      ),
+      GoRoute(
+        path: kReviewRatingScreen,
+        builder: (context, state) => const ReviewRatingScreen(),
       ),
     ],
   );
